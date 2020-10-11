5bb8bbbf1fe6cef86f7899fbe6d010d2574beb37
Abdullah SEN <sen-abdullah@yandex.com>
2020-10-11 16:47:15 +0200

    Implements spring security for authentication

--- src/main/java/com/abdullahsen/ws/user/User.java
+++ src/main/java/com/abdullahsen/ws/user/User.java
@@ -10,10 +10,15 @@ import javax.validation.constraints.Size;
 import com.abdullahsen.ws.shared.Views;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
 	
 	@Id
 	@GeneratedValue
@@ -38,6 +43,30 @@ public class User {
 
 	@JsonView(Views.Base.class)
 	private String image;
-	
-		
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList("Role_user");
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
